d4b7c0fc8f2104db6571c553861289543e64cda8
Ales Rechtorik <foxxcz@gmail.com>
2024-06-24 19:01:18 +0200

    asd

--- src/main/java/com/zerops/recipe/RecipeApplication.java
+++ src/main/java/com/zerops/recipe/RecipeApplication.java
@@ -2,8 +2,12 @@ package com.zerops.recipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.zerops.recipe.repository")
+@EntityScan(basePackages = "com.zerops.recipe.model")
 public class RecipeApplication {
 
     public static void main(String[] args) {
