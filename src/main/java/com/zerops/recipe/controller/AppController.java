e1385ca7049f89d85125d0c9cfd807ff75a8cf52
Ales Rechtorik <foxxcz@gmail.com>
2024-06-24 18:44:27 +0200

    fix dep

--- src/main/java/com/zerops/recipe/controller/AppController.java
+++ src/main/java/com/zerops/recipe/controller/AppController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 public class AppController {
 
